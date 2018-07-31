75cd0f700fca40cb9a7bd3c10a051a7a1dce9388
Horusofoz AIE <horusofoz@gmail.com>
2018-07-31 20:12:11 +0930

    * Added Shell prefab with nested shell explosion imported from AIE Instructure * Added script to shell * Created tank shooting script and attached to tank * Created enemy tank shooting script and attached to enemy tank * Created script for taking damage and updating health counter * Player and enemy set to inactive if health <= 0

---
+++ Tanks/Assets/Scripts/Tank/EnemyTankShooting.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTankShooting : MonoBehaviour {
+
+    // Prefab of the shell
+    public Rigidbody m_Shell;
+    // A child of the tank where the shells are spawned
+    public Transform m_FireTransform;
+
+    // The force fiven to the shell when firing
+    public float m_LaunchForce = 30f;
+
+    public float m_ShootDelay = 1f;
+
+    private bool m_CanShoot;
+    private float m_ShootTimer;
+
+    private void Awake()
+    {
+        m_CanShoot = false;
+    }
+
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(m_CanShoot == true)
+        {
+            m_ShootTimer -= Time.deltaTime;
+            if (m_ShootTimer <= 0)
+            {
+                m_ShootTimer = Time.deltaTime;
+                Fire();
+            }
+        }
+	}
+
+    private void Fire()
+    {
+        // Create an instance of the shell and store a reference to it's rigidbody
+        Rigidbody shellInstance = Instantiate(m_Shell, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;
+
+        // Set the shell's velocity to the launch force in the fire position's forward direction
+        shellInstance.velocity = m_LaunchForce * m_FireTransform.forward;
+
+        m_ShootTimer = m_ShootDelay;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            m_CanShoot = true;
+            m_ShootTimer = m_ShootDelay;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            m_CanShoot = false;
+        }
+    }
+}
---
+++ Tanks/Assets/Scripts/Tank/TankHealth.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankHealth : MonoBehaviour {
+
+    // The amoun of health each tank starts with
+    public float m_StartingHealth = 100f;
+
+    // A prefab that will be instantiated in Awake, then used whenever the tank dies
+    public GameObject m_ExplosionPrefab;
+
+    // Holder for camera
+    public Camera m_mainCamera;
+
+    private float m_CurrentHealth;
+    private bool m_Dead;
+    // The particle sstem that will play when the tank is destroyed
+    private ParticleSystem m_ExplosionParticles;
+
+    private void Awake()
+    {
+        // Instantiate the explosion prefab and get a reference to the particle system on it
+        m_ExplosionParticles = Instantiate(m_ExplosionPrefab).GetComponent<ParticleSystem>();
+
+        // Disable the prefab so it can be activated when it's required
+        m_ExplosionParticles.gameObject.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        // When the tank is enabled, reset the tank's health and whether or not it's dead
+        m_CurrentHealth = m_StartingHealth;
+        m_Dead = false;
+
+        SetHealthUI();
+    }
+
+    private void SetHealthUI()
+    {
+        // TODO: Update the user interface showing the tank's health
+    }
+
+    public void TakeDamage(float amount)
+    {
+        // Reduce current health by the amount of damage done
+        m_CurrentHealth -= amount;
+
+        // Change th eUI elements appropriately
+        SetHealthUI();
+
+        // If the current health is at or below zerio and it has not yet been registered, call OnDeath
+        if(m_CurrentHealth <= 0 && !m_Dead)
+        {
+            OnDeath();
+        }
+    }
+
+    private void OnDeath()
+    {
+        // Set the flag that this function is only called once
+        m_Dead = true;
+
+        // move the instantiated explosion prefab to the tank's position and turn it on
+        m_ExplosionParticles.transform.position = transform.position;
+        m_ExplosionParticles.gameObject.SetActive(true);
+
+        // Play the particle system of the tank exploding
+        m_ExplosionParticles.Play();
+
+        // Unparent Main Camera
+        if(m_mainCamera != null)
+        {
+            m_mainCamera.transform.parent = null;
+        }
+
+        // Turn the tank off
+        gameObject.SetActive(false);
+    }
+}
