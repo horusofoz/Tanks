c4088a7b99ef2e6946b058a9b025d58ebbbedc0e
Jeff <horusofoz@gmail.com>
2018-08-11 12:16:11 +0930

    Added basic camera zooming from scroll wheel Will need constraints added later

--- Tanks/Assets/Scripts/Camera/CameraControl.cs
+++ Tanks/Assets/Scripts/Camera/CameraControl.cs
@@ -8,12 +8,18 @@ public class CameraControl : MonoBehaviour {
 
     public Transform m_target;
 
+    public float scrollSize = 5f;
+
     private Vector3 m_MoveVelocity;
     private Vector3 m_DesiredPosition;
+
+    private Camera m_camera;
 
     private void Awake()
     {
         m_target = GameObject.FindGameObjectWithTag("Player").transform;
+        m_camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponentInChildren<Camera>();
+        
     }
 
     private void FixedUpdate()
@@ -21,9 +27,30 @@ public class CameraControl : MonoBehaviour {
         Move();
     }
 
+    private void Update()
+    {
+        ManageCameraZoom();
+    }
+
     private void Move()
     {
         m_DesiredPosition = m_target.position;
         transform.position = Vector3.SmoothDamp(transform.position, m_DesiredPosition, ref m_MoveVelocity, m_DampTime);
+    }
+
+    private void ManageCameraZoom()
+    {
+        // Get mouse scroll value
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        
+        // Is thge scroll value 0?
+        if(scroll == 0f)
+        {
+            return;
+        }
+
+        // Modify the orthographic size by the scroll value
+        m_camera.orthographicSize += scroll * scrollSize;
+        
     }
 }
